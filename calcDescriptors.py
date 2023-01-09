a3418234d78b302d4db2b532a5d8fb457ed10379
Matteo <matteo.bertola77@gmail.com>
2023-01-09 15:49:43 +0100

    added calcDescriptors.py

---
+++ calcDescriptors.py
@@ -0,0 +1,25 @@
+from rdkit import Chem
+import rdkit.Chem.Descriptors as Descriptors
+
+molecule = 'CCCO'
+rdkit_molecule = Chem.MolFromSmiles(molecule)
+
+molecular_weight = Descriptors.ExactMolWt(rdkit_molecule)
+logp = Descriptors.MolLogP(rdkit_molecule)
+h_bond_donor = Descriptors.NumHDonors(rdkit_molecule)
+h_bond_acceptors = Descriptors.NumHAcceptors(rdkit_molecule)
+rotatable_bonds = Descriptors.NumRotatableBonds(rdkit_molecule)
+number_of_atoms = Chem.rdchem.Mol.GetNumAtoms(rdkit_molecule)
+formal_charge = Chem.rdmolops.GetFormalCharge(rdkit_molecule)
+number_of_rings = Chem.rdMolDescriptors.CalcNumRings(rdkit_molecule)
+molar_refractivity = Chem.Crippen.MolMR(rdkit_molecule)
+
+print("Molecular Weight (g/mL): %s" % molecular_weight)
+print("Log P: %s" % logp)
+print("H Bond Donors: %s" % h_bond_donor)
+print("H Bond Acceptors: %s" % h_bond_donor)
+print("Rotatable Bonds: %s" % rotatable_bonds)
+print("Number of Atoms: %s" % number_of_atoms)
+print("Formal Charge: %s" % formal_charge)
+print("Number of Rings: %s" % number_of_rings)
+print("Molar Refractivity: %s" % molar_refractivity)
